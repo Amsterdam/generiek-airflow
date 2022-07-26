ee1b27021dce5c3608dcf97b9f129c2b122f1b5e
Hans Bakker <hbakker@xpirit.com>
2022-07-26 15:57:47 +0200

    use generated_postgres_conn_id

--- src/dags/testdag.py
+++ src/dags/testdag.py
@@ -4,6 +4,7 @@ from common import default_args
 from postgres_update_azure_token_operator import PostgresUpdateAzureTokenOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
+
 def create_error(*args, **kwargs):
     """Generic error raiser."""
     raise Exception
@@ -17,9 +18,11 @@ with DAG(
         "SELECT * FROM public.covid_19_alcoholverkoopverbod;",
     ]
     pg_update_azure_token_test = PostgresUpdateAzureTokenOperator(
-        task_id="pg_update_azure_token_test", postgres_conn_id="postgres_default"
+        task_id="pg_update_azure_token_test",
+        postgres_conn_id="postgres_default",
+        generated_postgres_conn_id="postgres_azure"
     )
     pgtest = PostgresOperator(
-        task_id="pgtest", postgres_conn_id="postgres_default", sql=sqls
+        task_id="pgtest", postgres_conn_id="postgres_azure", sql=sqls
     )
     pg_update_azure_token_test >> pgtest
