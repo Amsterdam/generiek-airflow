95e9a5e65743cc11fc6721a8bee75725565fed0c
Hans Bakker <hbakker@xpirit.com>
2022-07-28 15:36:58 +0200

    use PostgresOnAzureOperator

--- src/dags/testdag.py
+++ src/dags/testdag.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from common import default_args
 
 from postgres_update_azure_token_operator import PostgresUpdateAzureTokenOperator
+from postgres_on_azure_operator import PostgresOnAzureOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 
@@ -17,12 +18,33 @@ with DAG(
     sqls = [
         "SELECT * FROM public.covid_19_alcoholverkoopverbod;",
     ]
-    pg_update_azure_token_test = PostgresUpdateAzureTokenOperator(
-        task_id="pg_update_azure_token_test",
-        postgres_conn_id="postgres_default",
-        generated_postgres_conn_id="postgres_azure"
+
+    # This DAG demonstrates two routes:
+    # - use PostgresOnAzureOperator and PostgresOnAzureHook.
+    # - use PostgresUpdateAzureTokenOperator to generate a temporary connection
+    #   containing the token that can then be used by PostgresOperator
+
+    # use PostgresOnAzureOperator and PostgresOnAzureHook.
+    # PostgresOnAzureOperator will call PostgresOnAzureHook under the hood,
+    # and therefore it can use the postgres_default connection.
+    pg_azure_test = PostgresOnAzureOperator(
+        task_id="pg_azure_test", postgres_conn_id="postgres_default", sql=sqls
     )
-    pgtest = PostgresOperator(
-        task_id="pgtest", postgres_conn_id="postgres_azure", sql=sqls
-    )
-    pg_update_azure_token_test >> pgtest
+
+    pg_azure_test
+
+    # use PostgresUpdateAzureTokenOperator to generate a temporary connection
+    # containing the token that can then be used by PostgresOperator
+    # (this is a workaround so that modifying the PostgresHook is not needed)
+    # The vanilla PostgresOperator cannot use our custom PostgresOnAzureHook,
+    # and therefore it needs that patched connection.
+
+    # pg_update_azure_token_test = PostgresUpdateAzureTokenOperator(
+    #     task_id="pg_update_azure_token_test",
+    #     postgres_conn_id="postgres_default",
+    #     generated_postgres_conn_id="postgres_azure"
+    # )
+    # pgtest = PostgresOperator(
+    #     task_id="pgtest", postgres_conn_id="postgres_azure", sql=sqls
+    # )
+    # pg_update_azure_token_test >> pgtest
